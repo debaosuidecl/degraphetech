3ec2e524e915d90122f0015a8f0840c12580224d
Deba <debaosuidecl@gmail.com>
2018-11-10 19:47:04 +0100

    added build

---
+++ client/build/precache-manifest.59fd13e47a266263cb9b4b34ff2a8ef3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e52f04675c44af80f48b723a04bcb97",
+    "url": "/static/media/dtlogo.6e52f046.png"
+  },
+  {
+    "revision": "6336b011b3c5da9c1e8566e536865d5d",
+    "url": "/static/media/developer-table.6336b011.jpg"
+  },
+  {
+    "revision": "0afc125113adf53b55d87f2eb68ced93",
+    "url": "/static/media/blackwhite.0afc1251.jpg"
+  },
+  {
+    "revision": "5ffca2667e6e235b64e5bbf85d580203",
+    "url": "/static/media/ContactImage.5ffca266.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e93794ef0cd855985c7c",
+    "url": "/static/js/main.e93794ef.chunk.js"
+  },
+  {
+    "revision": "b396f7f54e8e47c33deb",
+    "url": "/static/js/1.b396f7f5.chunk.js"
+  },
+  {
+    "revision": "e93794ef0cd855985c7c",
+    "url": "/static/css/main.b0faea6c.chunk.css"
+  },
+  {
+    "revision": "f15e9c1d1580c58710c3c907b7696920",
+    "url": "/index.html"
+  }
+];
